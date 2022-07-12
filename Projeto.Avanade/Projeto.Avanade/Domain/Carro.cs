7779c51bf6b6f84bb2906b9f3fa97dd0024b0bfe
Nicolas Almeida <ng.almeidaa@gmail.com>
2022-07-12 10:28:26 -0300

    fix: corrigindo decorator da classe

--- Projeto.Avanade/Projeto.Avanade/Domain/Carro.cs
+++ Projeto.Avanade/Projeto.Avanade/Domain/Carro.cs
@@ -1,4 +1,6 @@
-﻿namespace Projeto.Avanade.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Avanade.Domain
 {
     /// <summary>
     /// Classe de Carro
@@ -6,8 +8,12 @@
     public class Carro
     {
         public int Id { get; set; }
+        [Required]
         public string Marca { get; set; }
+        [Required]
+        [Range(1990, 2023, ErrorMessage = "Por favor, insira um ano entre 1990 e 2023")]
         public int Ano { get; set; }
+        [Required]
         public bool Novo { get; set; }
 
     }
